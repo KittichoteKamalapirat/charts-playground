dcfc9527fcccaac13550d4d9e53fe2c31bf6e05e
KittichoteKamalapirat <kittichoteshane@gmail.com>
2023-08-28 21:21:54 +0700

    setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Recorder from "./components/Recorder";
 function App() {
   return (
     <div>
+      <p className="text-black text-4xl">test</p>
       <Recorder />
       <br />
     </div>
